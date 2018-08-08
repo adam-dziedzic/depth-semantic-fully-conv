ccba88d16bfc543bb0bf83600c1476b861a15322
adam-dziedzic <adam.dziedzi@gmail.com>
2018-08-08 18:07:37 -0500

    get to a running stage

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-import numpy as np
+import argparse
 import random
-import os
-import PIL
-from PIL import Image
-from model import *
-from torch.autograd import Variable
-from weights import load_weights
-from scipy import misc
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import torch.optim as optim
+import sys
 import torch.utils.data as data_utils
 import torchvision.transforms as transforms
-from torchvision import utils
+
 import flow_transforms
-import torch
-from nyu_dataset_loader import *
 from utils import *
+from weights import load_weights
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--is_debug", default=False, type=bool,
+                    help="is it the debug mode execution")
+parser.add_argument("-l", "--log_file", default="deeplens.log",
+                    help="The name of the log file.")
+parser.add_argument("-b", "--batch_size", default=16, type=int,
+                    help="the initial size of the batch (number of data points "
+                         "for a single forward and batch passes")
 
 input_rgb_images_dir = 'data/nyu_datasets_changed/input/'
 target_depth_images_dir = 'data/nyu_datasets_changed/target_depths/'
 target_labels_images_dir = 'data/nyu_datasets_changed/labels_38/'
 
-dtype = torch.cuda.FloatTensor ## change when running on CPU
+if torch.cuda.is_available():
+    print("CUDA is available")
+    dtype = torch.cuda.FloatTensor
+else:
+    print("No CUDA detected")
+    dtype = torch.FloatTensor
 
-train_on = 1000 
+args = parser.parse_args(sys.argv[1:])
+is_debug = args.is_debug
+log_file = args.log_file
+batch_size = args.batch_size
+
+train_on = 1000
 val_on = 100
 test_on = 50
-batch_size= 16
+
 
 print('Loading images...')
 
@@ -37,81 +44,89 @@ NUM_TRAIN = 1000
 NUM_VAL = 300
 NUM_TEST = 149
 
-listing = random.sample(os.listdir(input_rgb_images_dir),1449)
-train_listing = listing[:min(NUM_TRAIN,train_on)]
-val_listing = listing[NUM_TRAIN:min(NUM_VAL+NUM_TRAIN,val_on+NUM_TRAIN)]
-test_listing = listing[NUM_VAL+NUM_TRAIN:min(NUM_VAL+NUM_TRAIN+NUM_TEST,test_on+NUM_VAL+NUM_TRAIN)]
+listing = random.sample(os.listdir(input_rgb_images_dir), 1449)
+train_listing = listing[:min(NUM_TRAIN, train_on)]
+val_listing = listing[NUM_TRAIN:min(NUM_VAL + NUM_TRAIN, val_on + NUM_TRAIN)]
+test_listing = listing[NUM_VAL + NUM_TRAIN:min(NUM_VAL + NUM_TRAIN + NUM_TEST,
+                                               test_on + NUM_VAL + NUM_TRAIN)]
 
-data_dir = (input_rgb_images_dir,target_depth_images_dir,target_labels_images_dir)
+data_dir = (
+    input_rgb_images_dir, target_depth_images_dir, target_labels_images_dir)
 
-input_transform = transforms.Compose([flow_transforms.Scale(228),flow_transforms.ArrayToTensor()])
-target_depth_transform = transforms.Compose([flow_transforms.Scale_Single(228),flow_transforms.ArrayToTensor()])
+input_transform = transforms.Compose(
+    [flow_transforms.Scale(228), flow_transforms.ArrayToTensor()])
+target_depth_transform = transforms.Compose(
+    [flow_transforms.ScaleSingle(228), flow_transforms.ArrayToTensor()])
 target_labels_transform = transforms.Compose([flow_transforms.ArrayToTensor()])
 
+# Apply this transform on input, ground truth depth images and labeled images
 
-## Apply this transform on input, ground truth depth images and labeled images
+co_transform = flow_transforms.Compose([
+    flow_transforms.RandomCrop((480, 640)),
+    flow_transforms.RandomHorizontalFlip()
+])
 
-co_transform=flow_transforms.Compose([
-            flow_transforms.RandomCrop((480,640)),
-            flow_transforms.RandomHorizontalFlip()
-        ])
+# Splitting in train, val and test sets [No data augmentation on val and test,
+# only on train]
 
+train_dataset = ListDataset(data_dir, train_listing, input_transform,
+                            target_depth_transform, target_labels_transform,
+                            co_transform)
 
-## Splitting in train, val and test sets [No data augmentation on val and test, only on train]
+val_dataset = ListDataset(data_dir, val_listing, input_transform,
+                          target_depth_transform, target_labels_transform)
 
-train_dataset = ListDataset(data_dir,train_listing,input_transform,target_depth_transform,\
-                            target_labels_transform,co_transform)
-
-val_dataset = ListDataset(data_dir,val_listing,input_transform,target_depth_transform,\
-                            target_labels_transform)
-
-test_dataset = ListDataset(data_dir,test_listing,input_transform,target_depth_transform,\
-                            target_labels_transform)
+test_dataset = ListDataset(data_dir, test_listing, input_transform,
+                           target_depth_transform, target_labels_transform)
 
 print("Loading data...")
-train_loader = data_utils.DataLoader(train_dataset,batch_size,shuffle = True, drop_last=True)
-val_loader = data_utils.DataLoader(val_dataset,batch_size,shuffle = True, drop_last=True)
-test_loader = data_utils.DataLoader(test_dataset,batch_size,shuffle = True, drop_last=True)
+train_loader = data_utils.DataLoader(train_dataset, batch_size, shuffle=True,
+                                     drop_last=True)
+val_loader = data_utils.DataLoader(val_dataset, batch_size, shuffle=True,
+                                   drop_last=True)
+test_loader = data_utils.DataLoader(test_dataset, batch_size, shuffle=True,
+                                    drop_last=True)
 
 model = Model(ResidualBlock, UpProj_Block, batch_size)
 model.type(dtype)
 
 # Loading pretrained weights
-model.load_state_dict(load_weights(model,weights_file,dtype))
+model.load_state_dict(load_weights(model, weights_file, dtype))
 
 loss_fn = torch.nn.NLLLoss2d().type(dtype)
 
-# Uncomment When transfer learning the model parameters of the semantic segmentation branch
+# Uncomment When transfer learning the model parameters of the semantic
+# segmentation branch
 # for name, param in model.named_parameters():
-#     if name.startswith('up_conv5') or\
-#        name.startswith('conv4') or\
-#        name.startswith('bn4') or\
-#        name.startswith('conv5') or\
-#        name.startswith('bn5'):
+#     if name.startswith('up_conv5') or \
+#             name.startswith('conv4') or \
+#             name.startswith('bn4') or \
+#             name.startswith('conv5') or \
+#             name.startswith('bn5'):
 #         param.requires_grad = True
 #     else:
 #         param.requires_grad = False
 
-
-# Uncomment when fine tuning the model by allowing backpropogation through all layers of the model
+# Uncomment when fine tuning the model by allowing backpropogation through all
+# layers of the model
 for param in model.parameters():
     param.requires_grad = True
 
-
-# framework to define different learning rate for different set of parameters in the model
-## reduce learning rate when loss states to plateau
-#scheduler = ReduceLROnPlateau(optimizer, 'min') # set up scheduler
+# framework to define different learning rate for different set of parameters
+# in the model reduce learning rate when loss states to plateau
+# scheduler = ReduceLROnPlateau(optimizer, 'min') # set up scheduler
 
 # Train the entire model for a few more epochs, checking accuracy on the
 # train and validation sets after each epoch.
 
 loss_history = []
-num_epochs = 50
+num_epochs = 1
 train_acc_history = []
 val_acc_history = []
 epoch_history = []
 best_val_acc = 0
-# Depends on whether transfer learning (0.05 [Determined by overfitting on validation set]) or fine-tuning (1e-5)
+# Depends on whether transfer learning (0.05 [Determined by overfitting on
+# validation set]) or fine-tuning (1e-5)
 learning_rate = 1e-5
 start_epoch = 0
 
@@ -119,66 +134,75 @@ resume_from_file = True
 resume_file = 'model_best.pth.tar'
 resumed_file = False
 
-
-#print('Transfer learning the weights for uprojection blocks and last conv layer ...')
-#print('on {} training examples with a batch size of {} for {} epochs'.format(train_on,batch_size,num_epochs))
+print(
+    'Transfer learning the weights for uprojection blocks and last conv layer')
+print('on {} training examples with a batch size of {} for {} epochs'.format(
+    train_on, batch_size, num_epochs))
 
 if resume_from_file:
-        if os.path.isfile(resume_file):
-            print("=> loading checkpoint '{}'".format(resume_file))
-            checkpoint = torch.load(resume_file)
-            start_epoch = checkpoint['epoch']
-            best_val_acc = checkpoint['best_val_acc']
-            model.load_state_dict(checkpoint['state_dict'])
-            #optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(resume_file, checkpoint['epoch']))
-            resumed_file = True
-        else:
-            print("=> no checkpoint found at '{}'".format(resume_file))
+    if os.path.isfile(resume_file):
+        print("=> loading checkpoint '{}'".format(resume_file))
+        checkpoint = torch.load(resume_file)
+        start_epoch = checkpoint['epoch']
+        best_val_acc = checkpoint['best_val_acc']
+        model.load_state_dict(checkpoint['state_dict'])
+        # optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+              .format(resume_file, checkpoint['epoch']))
+        resumed_file = True
+    else:
+        print("=> no checkpoint found at '{}'".format(resume_file))
 
 for epoch in range(num_epochs):
 
-    # Uncomment When transfer learning the model parameters of the semantic segmentation branch
+    # Uncomment When transfer learning the model parameters of the semantic
+    # segmentation branch
 
     # optimizer = torch.optim.Adam([
-    #                 # {'params': model.up_conv5.parameters()},
-    #                 # {'params': model.conv4.parameters()},
-    #                 # {'params': model.bn4.parameters()},
-    #                 # {'params': model.bn5.parameters()},
-    #                 # {'params': model.conv5.parameters()}
-    #                 #{'params': model.classifier.parameters(), 'lr': 1e-3}
-    #             ], lr = learning_rate)
+    #     {'params': model.up_conv5.parameters()},
+    #     {'params': model.conv4.parameters()},
+    #     {'params': model.bn4.parameters()},
+    #     {'params': model.bn5.parameters()},
+    #     {'params': model.conv5.parameters()},
+    #     {'params': model.classifier.parameters(), 'lr': 1e-3}
+    # ], lr=learning_rate)
 
-    # Uncomment when fine tuning the model by allowing backpropogation through all layers of the model
-    optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+    # Uncomment when fine tuning the model by allowing backpropogation
+    # through all layers of the model
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
     print('Learning Rate for this epoch: {}'.format(learning_rate))
     loss = run_epoch(model, loss_fn, train_loader, optimizer, dtype)
-    #scheduler.step(loss, epoch) # update lr if loss plateaus (by a factor of 0.05, wait time is 4 epochs)
-    print('Loss for epoch {} : {}'.format(start_epoch+epoch+1,loss))
-    if epoch % 2 == 0 or epoch == num_epochs-1:
+    # update lr if loss plateaus (by a factor
+    # of 0.05, wait time is 4 epochs)
+    # scheduler.step(loss, epoch)
+    print('Loss for epoch {} : {}'.format(start_epoch + epoch + 1, loss))
+    if epoch % 2 == 0 or epoch == num_epochs - 1:
         if epoch % 8 == 0 and epoch != 0:
-            learning_rate = learning_rate*0.5
+            learning_rate = learning_rate * 0.5
         loss_history.append(loss)
-        epoch_history.append(start_epoch+epoch+1)
-        train_acc = check_accuracy(model,train_loader,start_epoch+epoch,dtype,visualize = False)
+        epoch_history.append(start_epoch + epoch + 1)
+        train_acc = check_accuracy(model, train_loader, start_epoch + epoch,
+                                   dtype, visualize=False)
         train_acc_history.append(train_acc)
-        print('Train accuracy for epoch {}: {} '.format(start_epoch+epoch + 1,train_acc))
-        val_acc = check_accuracy(model,val_loader,start_epoch+epoch,dtype,visualize = True)
+        print('Train accuracy for epoch {}: {} '.format(start_epoch + epoch + 1,
+                                                        train_acc))
+        val_acc = check_accuracy(model, val_loader, start_epoch + epoch, dtype,
+                                 visualize=True)
         val_acc_history.append(val_acc)
-        print('Validation accuracy for epoch {} : {} '.format(start_epoch+epoch + 1,val_acc))
-        plot_performance_curves(loss_history,train_acc_history,val_acc_history,epoch_history,\
-                                train_on,batch_size,num_epochs,resumed_file)
+        print('Validation accuracy for epoch {} : {} '.format(
+            start_epoch + epoch + 1, val_acc))
+        plot_performance_curves(loss_history, train_acc_history,
+                                val_acc_history, epoch_history, train_on,
+                                batch_size, num_epochs, resumed_file)
         is_best = val_acc > best_val_acc
-        best_val_acc = max(val_acc,best_val_acc)
+        best_val_acc = max(val_acc, best_val_acc)
         save_checkpoint({
-            'epoch': start_epoch+epoch + 1,
+            'epoch': start_epoch + epoch + 1,
             'state_dict': model.state_dict(),
             'best_val_acc': best_val_acc,
-            'optimizer' : optimizer.state_dict(),
+            'optimizer': optimizer.state_dict(),
         }, is_best)
 
-
-#test_acc = check_accuracy(model,test_loader,start_epoch+epoch,dtype,visualize = True)
-#print('Test accuracy: ', val_acc)
+# test_acc = check_accuracy(model,test_loader,start_epoch+epoch,dtype,visualize = True)
+# print('Test accuracy: ', val_acc)
--- utils.py
+++ utils.py
@@ -1,145 +1,157 @@
-import numpy as np
-import random
-import os
-import PIL
-from PIL import Image
-from model import *
-from torch.autograd import Variable
-from weights import load_weights
-from scipy import misc
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import torch.optim as optim
-import torch.utils.data as data_utils
-import torchvision.transforms as transforms
-from torchvision import utils
-import flow_transforms
-import torch
-from nyu_dataset_loader import *
-import warnings
-from torch.optim.optimizer import Optimizer
 import shutil
 
+from model import *
+from nyu_dataset_loader import *
+
+matplotlib.use('Agg')
+
 weights_file = "NYU_ResNet-UpProj.npy"
-color = np.array([(0,0,0),(0,0,255),(255,0,0),(0,255,0),(255,255,0),(255,0,255), #magenta
-        (192,192,192), #silver
-        (128,128,128), #gray
-        (128,0,0) ,#maroon
-        (128,128,0) ,#olive
-        (0,128,0) ,#green
-        (128,0,128), # purple
-        (0,128,128) , # teal
-        (65,105,225) , #royal blue
-        (255,250,205) , #lemon chiffon
-        (255,20,147) , #deep pink
-        (218,112,214) , #orchid]
-        (135,206,250) , #light sky blue
-        (127,255,212),  #aqua marine
-        (0,255,127) , #spring green
-        (255,215,0) , #gold
-        (165,42,42) , #brown
-        (148,0,211) , #violet
-        (210,105,30) , # chocolate
-        (244,164,96),  # sandy brown
-        (240,255,240),  #honeydew
-        (112,128,144), (64,224,208) ,(100,149,237) ,(30,144,255),(221,160,221),
-        (205,133,63),(255,240,245),(255,255,240),(255,165,0),(255,160,122),(205,92,92),(240,248,255)])
+color = np.array(
+    [(0, 0, 0), (0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 255, 0),
+     (255, 0, 255),  # magenta
+     (192, 192, 192),  # silver
+     (128, 128, 128),  # gray
+     (128, 0, 0),  # maroon
+     (128, 128, 0),  # olive
+     (0, 128, 0),  # green
+     (128, 0, 128),  # purple
+     (0, 128, 128),  # teal
+     (65, 105, 225),  # royal blue
+     (255, 250, 205),  # lemon chiffon
+     (255, 20, 147),  # deep pink
+     (218, 112, 214),  # orchid]
+     (135, 206, 250),  # light sky blue
+     (127, 255, 212),  # aqua marine
+     (0, 255, 127),  # spring green
+     (255, 215, 0),  # gold
+     (165, 42, 42),  # brown
+     (148, 0, 211),  # violet
+     (210, 105, 30),  # chocolate
+     (244, 164, 96),  # sandy brown
+     (240, 255, 240),  # honeydew
+     (112, 128, 144), (64, 224, 208), (100, 149, 237), (30, 144, 255),
+     (221, 160, 221),
+     (205, 133, 63), (255, 240, 245), (255, 255, 240), (255, 165, 0),
+     (255, 160, 122), (205, 92, 92), (240, 248, 255)])
 
 
 def run_epoch(model, loss_fn, loader, optimizer, dtype):
-  """
-  Train the model for one epoch.
-  """
-  # Set the model to training mode
-  #model.train()
-  running_loss = 0
-  count = 0
-  for x,z,y in loader:
+    """
+    Train the model for one epoch.
+    """
+    # Set the model to training mode
+    # model.train()
+    running_loss = 0
+    count = 0
+    for x, z, y in loader:
+        x_var = Variable(x.type(dtype))
+        z_var = Variable(z.type(dtype))
+        y_var = Variable(y.type(dtype).long())
+        m = nn.LogSoftmax()
 
-    x_var = Variable(x.type(dtype))
-    z_var = Variable(z.type(dtype))
-    y_var = Variable(y.type(dtype).long())
-    m = nn.LogSoftmax()
+        pred_depth, pred_labels = model(x_var, z_var)
+        y_var = y_var.squeeze()
 
-    pred_depth,pred_labels = model(x_var,z_var)
-    y_var = y_var.squeeze()
+        loss = loss_fn(m(pred_labels), y_var)
+        running_loss += loss.data.cpu().numpy()
+        count += 1
 
-    loss = loss_fn(m(pred_labels), y_var)
-    running_loss += loss.data.cpu().numpy()
-    count += 1
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-  return running_loss/count
-
-
-def check_accuracy(model, loader,epoch, dtype, visualize = False):
-  """
-  Check the accuracy of the model.
-  """
-
-  num_correct, num_samples = 0, 0
-  for x,z,y in loader:
-
-    x_var = Variable(x.type(dtype),volatile=True)
-    z_var = Variable(z.type(dtype),volatile=True)
+    return running_loss / count
 
 
-    pred_depth,pred_labels = model(x_var,z_var)
+def check_accuracy(model, loader, epoch, dtype, visualize=False):
+    """
+    Check the accuracy of the model.
+    """
 
-    _,preds = pred_labels.data.cpu().max(1)
-    if visualize == True:
-        
-        #Save the input RGB image, Ground truth depth map, Ground Truth Coloured Semantic Segmentation Map, 
-        #Predicted Coloured Semantic Segmentation Map, Predicted Depth Map for one image in the current batch
-        
-        input_rgb_image = x_var[0].data.permute(1,2,0).cpu().numpy().astype(np.uint8)
-        plt.imsave('input_rgb_epoch_{}.png'.format(epoch),input_rgb_image)
+    num_correct, num_samples = 0, 0
+    for x, z, y in loader:
 
-        input_gt_depth_image =  z_var[0].data.permute(1,2,0).cpu().numpy().astype(np.uint8)
-        plt.imsave('input_gt_depth_epoch_{}.png'.format(epoch),input_gt_depth_image)
+        x_var = Variable(x.type(dtype), volatile=True)
+        z_var = Variable(z.type(dtype), volatile=True)
 
-        colored_gt_label = color[y[0].squeeze().cpu().numpy().astype(int)].astype(np.uint8)
-        plt.imsave('gt_label_epoch_{}.png'.format(epoch),colored_gt_label)
+        pred_depth, pred_labels = model(x_var, z_var)
 
-        colored_pred_label = color[preds[0].squeeze().cpu().numpy().astype(int)].astype(np.uint8)
-        plt.imsave('pred_label_epoch_{}.png'.format(epoch),colored_pred_label)
+        _, preds = pred_labels.data.cpu().max(1)
+        if visualize == True:
+            # Save the input RGB image, Ground truth depth map, Ground Truth
+            # Coloured Semantic Segmentation Map,
+            # Predicted Coloured Semantic Segmentation Map, Predicted Depth Map
+            # for one image in the current batch
 
-        pred_depth_image = pred_depth[0].data.squeeze().cpu().numpy().astype(np.uint8)
-        plt.imsave('pred_depth_epoch_{}.png'.format(epoch),pred_depth_image,cmap = "gray")
+            input_rgb_image = x_var[0].data.permute(1, 2,
+                                                    0).cpu().numpy().astype(
+                np.uint8)
+            plt.imsave('input_rgb_epoch_{}.png'.format(epoch), input_rgb_image)
 
-    # Computing pixel-wise accuracy    
-    num_correct += (preds.long() == y.long()).sum()
-    num_samples += preds.numel()
+            input_gt_depth_image = z_var[0].data.permute(1, 2,
+                                                         0).cpu().numpy().astype(
+                np.uint8)
+            plt.imsave('input_gt_depth_epoch_{}.png'.format(epoch),
+                       input_gt_depth_image)
 
-  acc = float(num_correct) / num_samples
-  return acc
+            colored_gt_label = color[
+                y[0].squeeze().cpu().numpy().astype(int)].astype(np.uint8)
+            plt.imsave('gt_label_epoch_{}.png'.format(epoch), colored_gt_label)
 
-def plot_performance_curves(loss_history,train_acc_history,val_acc_history,epoch_history,train_on,batch_size,num_epochs,resumed_file):
-        plt.figure()
-        plt.plot(np.array(epoch_history),np.array(loss_history))
-        plt.ylabel('Loss')
-        plt.xlabel('Number of Epochs')
-        plt.title('Loss history for training model on {} examples with batch size of {}'.format(train_on,batch_size))
-        if resumed_file == False:
-            plt.savefig('loss_plot_train_on_{}_batch_size_{}.png'.format(train_on,batch_size))
-        else:
-            plt.savefig('loss_plot_train_on_{}_batch_size_{}_resumed.png'.format(train_on,batch_size))
-        plt.figure()
-        plt.plot(np.array(epoch_history),np.array(train_acc_history),label = 'Training accuracy')
-        plt.plot(np.array(epoch_history),np.array(val_acc_history), label = 'Validation accuracy')
-        plt.title('Accuracy history for training model on {} examples with batch size of {}'.format(train_on,batch_size))
-        plt.ylabel('Accuracy')
-        plt.xlabel('Number of Epochs')
-        plt.legend()
-        if resumed_file == False:
-            plt.savefig('acc_plots_train_on_{}_batch_size_{}.png'.format(train_on,batch_size))
-        else:
-            plt.savefig('acc_plots_train_on_{}_batch_size_{}_resumed.png'.format(train_on,batch_size))
+            colored_pred_label = color[
+                preds[0].squeeze().cpu().numpy().astype(int)].astype(np.uint8)
+            plt.imsave('pred_label_epoch_{}.png'.format(epoch),
+                       colored_pred_label)
+
+            pred_depth_image = pred_depth[
+                0].data.squeeze().cpu().numpy().astype(np.uint8)
+            plt.imsave('pred_depth_epoch_{}.png'.format(epoch),
+                       pred_depth_image, cmap="gray")
+
+        # Computing pixel-wise accuracy
+        num_correct += (preds.long() == y.long()).sum()
+        num_samples += preds.numel()
+
+    acc = float(num_correct) / num_samples
+    return acc
+
+
+def plot_performance_curves(loss_history, train_acc_history, val_acc_history,
+                            epoch_history, train_on, batch_size, num_epochs,
+                            resumed_file):
+    plt.figure()
+    plt.plot(np.array(epoch_history), np.array(loss_history))
+    plt.ylabel('Loss')
+    plt.xlabel('Number of Epochs')
+    plt.title(
+        'Loss history for training model on {} examples with batch size of {}'.format(
+            train_on, batch_size))
+    if resumed_file == False:
+        plt.savefig('loss_plot_train_on_{}_batch_size_{}.png'.format(train_on,
+                                                                     batch_size))
+    else:
+        plt.savefig(
+            'loss_plot_train_on_{}_batch_size_{}_resumed.png'.format(train_on,
+                                                                     batch_size))
+    plt.figure()
+    plt.plot(np.array(epoch_history), np.array(train_acc_history),
+             label='Training accuracy')
+    plt.plot(np.array(epoch_history), np.array(val_acc_history),
+             label='Validation accuracy')
+    plt.title(
+        'Accuracy history for training model on {} examples with batch size of {}'.format(
+            train_on, batch_size))
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of Epochs')
+    plt.legend()
+    if resumed_file == False:
+        plt.savefig('acc_plots_train_on_{}_batch_size_{}.png'.format(train_on,
+                                                                     batch_size))
+    else:
+        plt.savefig(
+            'acc_plots_train_on_{}_batch_size_{}_resumed.png'.format(train_on,
+                                                                     batch_size))
+
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
